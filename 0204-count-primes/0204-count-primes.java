29fd48b2390a2445c33d10e2fc9e1e95552df800
PALAKJAIN26 <palak.ajmera256@gmail.com>
2026-07-04 23:37:42 +0530

    Time: 53 ms (98.46%), Space: 51.1 MB (71.74%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,6 +1,5 @@
 class Solution {
     public int countPrimes(int n) {
-        // There are no primes less than 2
         if (n <= 2) {
             return 0;
         }
@@ -12,9 +11,7 @@ class Solution {
             if (!isComposite[i]) {
                 count++;
                 
-                // Cross out multiples starting from i^2.
                 if ((long) i * i < n) {
-                   
                     for (int j = i * i; j < n; j += 2 * i) {
                         isComposite[j] = true;
                     }
