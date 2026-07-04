6b2eae234cd4011fc8937cfd11b702e73d09e71d
PALAKJAIN26 <palak.ajmera256@gmail.com>
2026-07-04 23:36:17 +0530

    Time: 53 ms (98.46%), Space: 51.2 MB (53.72%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,23 +1,21 @@
 class Solution {
     public int countPrimes(int n) {
-        // A prime number is greater than 1. 
-        // If n is 0, 1, or 2, there are strictly 0 primes less than n.
+        // There are no primes less than 2
         if (n <= 2) {
             return 0;
         }
         
-        // boolean arrays default to false in Java. 
-        // We track if a number is a composite (non-prime).
+        int count = 1; 
         boolean[] isComposite = new boolean[n];
-        int count = 0;
         
-        for (int i = 2; i < n; i++) {
-            // If the number hasn't been crossed out, it's a prime
+        for (int i = 3; i < n; i += 2) {
             if (!isComposite[i]) {
                 count++;
                 
+                // Cross out multiples starting from i^2.
                 if ((long) i * i < n) {
-                    for (int j = i * i; j < n; j += i) {
+                   
+                    for (int j = i * i; j < n; j += 2 * i) {
                         isComposite[j] = true;
                     }
                 }
