9023255f086d166a69e383d249ab0030b39c5fdd
PALAKJAIN26 <palak.ajmera256@gmail.com>
2026-08-04 00:08:47 +0530

    Time: 13 ms (30.91%), Space: 93.2 MB (7.33%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                set.add(nums[i]);
+            }
+        }
+
+        int target = 1;
+        while (set.contains(target)) {
+            target++;
+        }
+
+        return target;
+    }
+}
