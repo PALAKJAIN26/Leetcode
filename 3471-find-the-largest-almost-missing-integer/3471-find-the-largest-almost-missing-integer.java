4a16f9df467d9c600ef68c6ba54321f9c2f37839
PALAKJAIN26 <palak.ajmera256@gmail.com>
2026-08-18 23:44:20 +0530

    Time: 6 ms (37.19%), Space: 46.2 MB (37.89%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,14 +1,8 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
         Map<Integer, Integer> windowCount = new HashMap<>();
 
-        // Slide window of size k across the array
         for (int i = 0; i <= n - k; i++) {
             Set<Integer> uniqueInWindow = new HashSet<>();
             for (int j = i; j < i + k; j++) {
@@ -20,7 +14,6 @@ class Solution {
             }
         }
 
-        // Find the largest value present in exactly 1 window
         int maxVal = -1;
         for (Map.Entry<Integer, Integer> entry : windowCount.entrySet()) {
             if (entry.getValue() == 1) {
